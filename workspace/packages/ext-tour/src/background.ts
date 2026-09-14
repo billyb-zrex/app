df1b018869ee51c6b8202bf33cf2ff0d8c540960
billyb-zrex <billy@zrexsolutions.com>
2026-09-14 13:11:11 -0600

    Remove browser cookie collection and remote font dependency from demo capture

--- workspace/packages/ext-tour/src/background.ts
+++ workspace/packages/ext-tour/src/background.ts
@@ -385,25 +385,6 @@ function getFavourableScreenDimension(tab: chrome.tabs.Tab) {
   });
 }
 
-async function getAllCookies(): Promise<chrome.cookies.Cookie[]> {
-  let cookies: chrome.cookies.Cookie[] = [];
-  let hasErr = false;
-  try {
-    // Support for Cookies Having Independent Partitioned State
-    // https://developers.google.com/privacy-sandbox/cookies/chips
-    cookies = (await (chrome.cookies.getAll as any)({ partitionKey: {} })) as chrome.cookies.Cookie[];
-  } catch (err) {
-    hasErr = true;
-    sentryCaptureException(err as Error);
-  }
-
-  if (hasErr || cookies.length === 0) {
-    cookies = await chrome.cookies.getAll({});
-  }
-
-  return cookies;
-}
-
 /**
  * This is how auto stitching of screens works based on user interaction
  *
@@ -625,7 +606,7 @@ chrome.runtime.onMessage.addListener(async (msg: MsgPayload<any>, sender) => {
         break;
       }
       const data = await chrome.storage.local.get(SCREEN_DATA_FINISHED);
-      const cookies = process.env.REACT_APP_CAPTURE_COOKIES === "false" ? [] : await getAllCookies();
+      const cookies: never[] = [];
       const screenStyleData: ThemeStats = (await chrome.storage.local.get(SCREEN_STYLE_DATA))[SCREEN_STYLE_DATA] || {};
 
       await chrome.tabs.sendMessage(sender.tab.id!, {
